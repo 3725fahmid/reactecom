0860c52f2e224888d0f6728d18fe5ba07af490be
fahmid <fahmidhasankonok@gmail.com>
2026-01-31 15:54:35 +0600

    add mid banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Carousel } from '../components/Carousel'
+import { MidBanner } from '../components/MidBanner'
 
 function Home() {
   return (
     <>
       <Carousel />
+      <MidBanner />
     </>
   )
 }
