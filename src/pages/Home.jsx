512127bb10f6fd79d0286228c5a2e001e4cb79fb
fahmid <fahmidhasankonok@gmail.com>
2026-01-31 16:00:10 +0600

    add features

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Carousel } from '../components/Carousel'
 import { MidBanner } from '../components/MidBanner'
 import Footer from '../components/Footer'
+import Features from '../components/Features'
 
 function Home() {
   return (
     <>
       <Carousel />
       <MidBanner />
+      <Features />
       <Footer />
     </>
   )
