d53f194e38fd31dd9359cb4fe56d985489eeaff2
fahmid <fahmidhasankonok@gmail.com>
2026-01-31 15:56:43 +0600

    add footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Carousel } from '../components/Carousel'
 import { MidBanner } from '../components/MidBanner'
+import Footer from '../components/Footer'
 
 function Home() {
   return (
     <>
       <Carousel />
       <MidBanner />
+      <Footer />
     </>
   )
 }
